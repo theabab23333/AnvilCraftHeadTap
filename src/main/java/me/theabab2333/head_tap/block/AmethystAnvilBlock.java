6c2d34020f15ecfbeb31327e71b1b64b90237707
theabab2333 <2047477146@qq.com>
2025-05-17 23:49:05 +0800

    修复紫水晶砧的问题
    修复了紫水晶砧被上方的磁铁被更新的时会被重复吸起(描述困难户qwq

--- src/main/java/me/theabab2333/head_tap/block/AmethystAnvilBlock.java
+++ src/main/java/me/theabab2333/head_tap/block/AmethystAnvilBlock.java
@@ -1,42 +1,23 @@
 package me.theabab2333.head_tap.block;
 
-import com.mojang.serialization.MapCodec;
-
-import net.minecraft.ChatFormatting;
+import dev.dubhe.anvilcraft.block.better.BetterAnvilBlock;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.FallingBlockEntity;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.FallingBlock;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
-import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.List;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
-public class AmethystAnvilBlock extends FallingBlock {
-    public static final MapCodec<AmethystAnvilBlock> CODEC = simpleCodec(AmethystAnvilBlock::new);
-    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+public class AmethystAnvilBlock extends BetterAnvilBlock {
     private static final VoxelShape BASE = Block.box(2.0, 0.0, 2.0, 14.0, 4.0, 14.0);
     private static final VoxelShape X_LEG = Block.box(4.0, 4.0, 6.0, 12.0, 10.0, 10.0);
     private static final VoxelShape X_TOP = Block.box(0.0, 10.0, 3.0, 16.0, 16.0, 13.0);
@@ -47,82 +28,32 @@ public class AmethystAnvilBlock extends FallingBlock {
 
     public AmethystAnvilBlock(Properties properties) {
         super(properties);
-        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
     @Override
-    protected @NotNull MapCodec<? extends FallingBlock> codec() {
-        return CODEC;
-    }
-
-    @Override
-    public BlockState getStateForPlacement(@NotNull BlockPlaceContext context) {
-        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getClockWise());
-    }
-
-    protected @NotNull VoxelShape getShape(
-            @NotNull BlockState state,
-            @NotNull BlockGetter level,
-            @NotNull BlockPos pos,
-            @NotNull CollisionContext context
-    ) {
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         Direction direction = state.getValue(FACING);
-        return direction.getAxis() == Direction.Axis.X ? X_AXIS_AABB : Z_AXIS_AABB;
+        if (direction.getAxis() == Direction.Axis.X) {
+            return X_AXIS_AABB;
+        }
+        return Z_AXIS_AABB;
     }
 
-    @Override
-    public void appendHoverText(
-            ItemStack pStack, Item.TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
-        super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
-        pTooltipComponents.add(Component.translatable("Pretty and pleasing to the ear")
-                .withStyle(ChatFormatting.GRAY));
+    protected void falling(FallingBlockEntity entity) {
+        entity.setHurtsEntities(2.0F, 10);
     }
 
-    protected void falling(@NotNull FallingBlockEntity fallingEntity) {
-        fallingEntity.setHurtsEntities(1.5F, 20);
-    }
-
-    @Override
-    public void onLand(
-            @NotNull Level level,
-            @NotNull BlockPos pos,
-            @NotNull BlockState state,
-            @NotNull BlockState replaceableState,
-            @NotNull FallingBlockEntity fallingBlock
-    ) {
+    public void onLand(Level level, BlockPos pos, BlockState state, BlockState replaceableState, FallingBlockEntity fallingBlock) {
         if (!fallingBlock.isSilent()) {
             level.levelEvent(1031, pos, 0);
         }
+
     }
 
-    public void onBrokenAfterFall(@NotNull Level level, @NotNull BlockPos pos, @NotNull FallingBlockEntity fallingBlock) {
+    public void onBrokenAfterFall(Level level, BlockPos pos, FallingBlockEntity fallingBlock) {
         if (!fallingBlock.isSilent()) {
             level.levelEvent(1029, pos, 0);
         }
-    }
 
-    @Override
-    public @NotNull DamageSource getFallDamageSource(@NotNull Entity entity) {
-        return entity.damageSources().anvil(entity);
-    }
-
-    @Override
-    protected @NotNull BlockState rotate(@NotNull BlockState state, @NotNull Rotation rot) {
-        return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
-    }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.@NotNull Builder<Block, BlockState> builder) {
-        builder.add(FACING);
-    }
-
-    @Override
-    protected boolean isPathfindable(@NotNull BlockState state, @NotNull PathComputationType pathComputationType) {
-        return false;
-    }
-
-    @Override
-    public int getDustColor(@NotNull BlockState state, @NotNull BlockGetter reader, @NotNull BlockPos pos) {
-        return state.getMapColor(reader, pos).col;
     }
 }
